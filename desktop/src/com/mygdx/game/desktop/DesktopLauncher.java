8dc5b7a0c548e33ec8a4027202c41b222ba44d80
jshen94 <jshen94@gmail.com>
2015-09-30 17:06:36 -0400

    git test - justin
    test

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MainClass;
 
-public class DesktopLauncher {
+public class DesktopLauncher { //git test -justin
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1100;
